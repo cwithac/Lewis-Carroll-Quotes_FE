187d559c11349246af6398d622b2037be6a0b756
Cathleen Wright <cathleenmwright@gmail.com>
2017-10-21 22:46:09 -0400

    Testing get request for characters in console.log

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,22 @@
 // console.log('app.js is linked to index.html for LC Quotes');
 
 const app = angular.module('quotes-app', []);
+
+angular.module('quotes-app').controller('characterController', ['$http', function($http){
+
+//TOGGLE FOR LOCAL-LIVE
+this.baseURL = 'http://localhost:3000/';
+// this.baseURL = 'HEROKU'
+
+this.getCharacters = function() {
+  $http({
+    method: 'GET',
+    url: this.baseURL + 'characters/'
+  }).then(function(response){
+    console.log(response.data);
+  })
+}
+
+this.getCharacters();
+
+}]);// End of characterController
