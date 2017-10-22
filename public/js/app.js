6123f73bdfee9e3e41fdfc3e93ca79cab0a8dff0
Cathleen Wright <cathleenmwright@gmail.com>
2017-10-21 22:49:44 -0400

    Code commenting

--- public/js/app.js
+++ public/js/app.js
@@ -4,18 +4,30 @@ const app = angular.module('quotes-app', []);
 
 angular.module('quotes-app').controller('characterController', ['$http', function($http){
 
+//------------------------------------------------------
 //TOGGLE FOR LOCAL-LIVE
+//------------------------------------------------------
 this.baseURL = 'http://localhost:3000/';
 // this.baseURL = 'HEROKU'
 
+//------------------------------------------------------
+//VARIABLES
+//------------------------------------------------------
+
+
+//------------------------------------------------------
+//INDEX ROUTE FOR ALL CHARACTERS
+//------------------------------------------------------
 this.getCharacters = function() {
   $http({
     method: 'GET',
     url: this.baseURL + 'characters/'
   }).then(function(response){
     console.log(response.data);
-  })
-}
+  }.bind(this));
+};
+
+
 
 this.getCharacters();
 
